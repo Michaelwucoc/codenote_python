6b0410d2a8a3a902558204b938187edc6c11ac5d
Michaelwucoc <goodluckmichael0@163.com>
2023-11-22 23:45:16 +0800

    Update Des

--- Exercises/Exercise 1 - 4 | Calculate.py
+++ Exercises/Exercise 1 - 4 | Calculate.py
@@ -5,12 +5,12 @@ Python: Area of a Circle
 
 
 """
-from math import pi
+from math import pi #Import the pi module from math
 
-r = float(input("Input the R of the circle"))
+r = float(input("Input the R of the circle")) #Ask user to enter the value
 
-area = pi * r ** 2
+area = pi * r ** 2 # Calculate the value
 
 print(
-    f"The area of the circle is{str(area)}, and you have input the R with the value of{r}"
+    f"The area of the circle is{str(area)}, and you have input the R with the value of{r}" # Print the value and show in console.
 )
--- Exercises/Exercise 1 - 5| Input Strings and print.py
+++ Exercises/Exercise 1 - 5| Input Strings and print.py
@@ -1,4 +1,4 @@
-first_name = input("Please enter your First Name:")
-last_name = input ("Please enter your Last Name:")
+first_name = input("Please enter your First Name:") # Let user enter their first name
+last_name = input ("Please enter your Last Name:") # Let user enter their last name
 
-print ("Hello,",last_name,first_name)
+print ("Hello,",last_name,first_name) # Use print to show the result
