2d4ca4cd405715ebd95ac79cd6b012caa639f92a
Michaelwucoc <goodluckmichael0@163.com>
2023-11-22 21:49:55 +0800

    Add more num

--- [S] 8 Num Ramdom Number Gen.py
+++ [S] 8 Num Ramdom Number Gen.py
@@ -1,5 +1,14 @@
 import random
-
+b= random.randint (10000000,99999999)
+c= random.randint (10000000,99999999)
+a= random.randint (10000000,99999999)
 x = random.randint (10000000,99999999)
+y = random.randint (10000000,99999999)
+z = random.randint (10000000,99999999)
 
 print (x)
+print (y)
+print (z)
+print (a)
+print (b)
+print (c)
