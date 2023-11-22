2aa16e50b3abc1ccf43f33c3a34574d33fe00177
Michaelwucoc <goodluckmichael0@163.com>
2023-11-22 22:05:33 +0800

    Update

---
+++ Exercises/Exercise 1 - 1.py
@@ -0,0 +1,13 @@
+"""
+    Write a Python program to print the following string in a specific format (see the output).
+Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are"
+Output :
+Twinkle, twinkle, little star,
+	How I wonder what you are! 
+		Up above the world so high,   		
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are
+"""
+
+print ("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\tHow I wonder where you are")
--- [Judge] Check the pass.py
+++ [Judge] Check the pass.py
@@ -1,10 +1,10 @@
-i = 0
-x = 12345678
+i = 0 #进入循环
+x = 12345678 #密码
 while i == 0:
-    y = int (input("Please enter the password. \n"))
-    
-    if x == y:
-        print ("Logged in to the server.")
-        break
+    y = int (input("Please enter the password. \n")) #要求用户输入
+    if x == y: #判断
+        print ("Logged in to the server.") #成功输出
+        break #退出程序
     else:
-        print ("Password incorrect. Please try again.")
+        print ("Password incorrect. Please try again.") #输入失败
+        
