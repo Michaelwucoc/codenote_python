36224b6f172f9caf51489a851623d0b9be45044b
Michaelwucoc <goodluckmichael0@163.com>
2023-11-22 22:10:29 +0800

    Update

---
+++ Exercises/Exercise 1 - 2.py
@@ -0,0 +1,12 @@
+"""
+Write a Python program to find out what version of Python you are using.
+
+A string containing the version number of the Python interpreter plus additional information on the build number and compiler used. This string is displayed when the interactive interpreter is started.
+
+"""
+import sys
+
+print ("Python Version")
+print(sys.version)
+print("Version Info")
+print(sys.version_info)
