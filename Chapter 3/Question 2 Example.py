6e85c3f06736c9211ad4858af0e33726e582147c
Michaelwucoc <goodluckmichael0@163.com>
2023-11-28 08:46:44 +0800

    Update a file

---
+++ Chapter 3/Question 2 Example.py
@@ -0,0 +1,16 @@
+import time
+rain = 0 # Outputs "1" when it's raining and "0" when it's not
+soil = 1 # Outputs "1" when the soil is dry and "0" when it's moist
+timer = 1 # Outputs "1" at specific times of the day to water the crops
+startWatering = 0 # 0 for turn off the watering system
+if timer and soil and not rain:
+    startWatering = 1
+else:
+    startWatering = 0
+if startWatering:
+    print('NOW the watering system is ON!!')
+wateringCounter = 0
+while wateringCounter < 11:
+    print('###### Watering the crops now ######')
+time.sleep(100)
+wateringCounter += 1
